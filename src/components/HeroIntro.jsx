d6e238b07e2927f4500aa645827215935bebf4d5
hannibal1631 <mukherjeerupak860@gmail.com>
2025-07-16 20:59:49 +0530

    remove hover effect

--- src/components/HeroIntro.jsx
+++ src/components/HeroIntro.jsx
@@ -1,12 +1,12 @@
 function HeroIntro({hero}) {
     return (
-      <div className='md:w-[35%] md:h-full w-full h-[30%] rounded-2xl relative group overflow-hidden'>
+      <div className='md:w-[35%] md:h-full w-full h-[30%] rounded-2xl relative overflow-hidden'>
         <img
           src={hero.image.url}
           alt={hero.name}
-          className='w-full h-full rounded-2xl transition-all duration-300'
+          className='w-full h-full rounded-2xl'
         />
-        <div className='absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black via-black/80 to-transparent text-white text-lg space-y-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-2xl'>
+        <div className='absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black via-black/80 to-transparent text-white text-lg space-y-1 opacity-100 rounded-b-2xl'>
           <p>
             <span className='font-semibold'>Name:</span> {hero.name}
           </p>
