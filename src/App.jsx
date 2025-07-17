fabc7f3ef0df98995b4b5980b303cff001590f57
hannibal1631 <mukherjeerupak860@gmail.com>
2025-07-17 20:30:43 +0530

    fix minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './index.css';
 import HeroCard from './components/HeroCard';
 import Search from './components/Search';
+import { useState } from 'react';
 
 function App() {
+  const [heroId, setHeroId] = useState(222); // Default hero ID
+
   return (
-    <>
-      <div className='bg-indigo-950 h-full w-full absolute flex justify-center items-center'>
-        <Search />
-        <HeroCard heroId={222} />
-      </div>
-    </>
+    <div className='bg-indigo-950 h-full w-full absolute flex justify-center items-center flex-col gap-4 p-4'>
+      <Search setHeroId={setHeroId} />
+      <HeroCard heroId={heroId} />
+    </div>
   );
 }
 
