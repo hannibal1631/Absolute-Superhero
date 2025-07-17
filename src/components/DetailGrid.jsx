d46c3500917dfaf57abe6100ad3cfbc9a3e6b74f
hannibal1631 <mukherjeerupak860@gmail.com>
2025-07-17 20:59:00 +0530

    change text colors

--- src/components/DetailGrid.jsx
+++ src/components/DetailGrid.jsx
@@ -1,14 +1,16 @@
-function DetailGrid({stats}) {
-    return (
-      <div className='flex flex-col gap-3'>
-        {Object.entries(stats).map(([key, value]) => (
-          <div key={key}>
-            <h3 className='capitalize md:text-2xl md:font-bold'>{key.replace(/-/g, ' ')}</h3>
-            <p className="md:text-xl">{value}</p>
-          </div>
-        ))}
-      </div>
-    );
+function DetailGrid({ stats }) {
+  return (
+    <div className='flex flex-col gap-3'>
+      {Object.entries(stats).map(([key, value]) => (
+        <div key={key}>
+          <h3 className='capitalize md:text-2xl md:font-bold text-zinc-400'>
+            {key.replace(/-/g, ' ')}
+          </h3>
+          <p className='md:text-xl'>{value}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default DetailGrid
+export default DetailGrid;
--- src/components/StatsGrid.jsx
+++ src/components/StatsGrid.jsx
@@ -1,8 +1,8 @@
 function StatsGrid({ title, stats }) {
   return (
     <div>
-      <h2 className="capitalize md:text-2xl md:font-bold">{title}</h2>
-      <div className='flex md:flex-row gap-7 text-center flex-wrap'>
+      <h2 className="capitalize md:text-2xl md:font-bold text-zinc-400">{title}</h2>
+      <div className='flex md:flex-row gap-5 text-center flex-wrap'>
         {Object.entries(stats).map(([key, value]) => (
           <div key={key}>
             <h3 className='capitalize md:text-lg md:font-semibold'>{key.replace(/-/g, ' ')}</h3>
